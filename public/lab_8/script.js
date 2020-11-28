6d2bad800c52615c816908aede6f4ec6cdec52a9
lilhenry <lillian.f.henry99@gmail.com>
2020-11-28 13:55:24 -0500

    tutorial instructions followed

---
+++ public/lab_8/script.js
@@ -0,0 +1,95 @@
+function convertRestaurantsToCategories(restaurantList) {
+  const list = restaurantList.reduce((collection, item, i) => {
+    const findcategory = collection.find((f) => f.label === item.category);
+    if (!findcategory) {
+      collection.push({
+        y: 1,
+        label: item.category
+      });
+    } else {
+      findcategory.y += 1;
+    }
+    return collection;
+  }, []);
+  return list;
+}
+
+function makeYourOptionsObject(datapointsFromRestaurantsList) {
+  // set your chart configuration here!
+  CanvasJS.addColorSet('customcolors', [
+    // done: add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
+    '#3B5761',
+    '#598391',
+    '#6DA1B3'
+  ]);
+
+  return {
+    animationEnabled: true,
+    colorSet: 'customcolors',
+    title: {
+      text: 'Places To Eat Out In Future'
+    },
+    axisX: {
+      interval: 1,
+      labelFontSize: 12
+    },
+    axisY2: {
+      interlacedColor: 'rgba(1,77,101,.2)',
+      gridColor: 'rgba(1,77,101,.1)',
+      title: 'Restaurants By Category',
+      labelFontSize: 12,
+      scaleBreaks: {
+        customBreaks: [
+          {
+            startValue: 40,
+            endValue: 50,
+            color: 'black'
+          },
+          {
+            startValue: 85,
+            endValue: 100,
+            color: 'black'
+          },
+          {
+            startValue: 140,
+            endValue: 175,
+            color: 'black'
+          }
+        ]
+      } // done: Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+    },
+    data: [{
+      type: 'bar',
+      name: 'restaurants',
+      axisYType: 'secondary',
+      dataPoints: datapointsFromRestaurantsList
+    }]
+  };
+}
+
+function runThisWithResultsFromServer(jsonFromServer) {
+  console.log('jsonFromServer', jsonFromServer);
+  sessionStorage.setItem('restaurantList', JSON.stringify(jsonFromServer)); // don't mess with this, we need it to provide unit testing support
+  const reorganizedData = convertRestaurantsToCategories(jsonFromServer);
+  const options = makeYourOptionsObject(reorganizedData);
+  const chart = new CanvasJS.Chart('chartContainer', options);
+  chart.render();
+}
+
+// Leave lines 52-67 alone; do your work in the functions above
+document.body.addEventListener('submit', async (e) => {
+  e.preventDefault(); // this stops whatever the browser wanted to do itself.
+  const form = $(e.target).serializeArray();
+  fetch('/sql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(form)
+  })
+    .then((fromServer) => fromServer.json())
+    .then((jsonFromServer) => runThisWithResultsFromServer(jsonFromServer))
+    .catch((err) => {
+      console.log(err);
+    });
+});
--- server.js
+++ server.js
@@ -4,12 +4,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
+import { open } from 'sqlite';
+import sqlite3 from 'sqlite3';
 import countries from './public/lab_6/countries.js';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const dbSettings = {
+  filename: './tmp/database.db',
+  driver: sqlite3.Database
+};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,6 +26,52 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
+// functions
+
+async function query(db) {
+  const result = await db.all('SELECT category, COUNT(restaurant_name) FROM restaurants GROUP BY category');
+  return result;
+}
+
+async function dataFetch() {
+  const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  const response = await fetch(url);
+  return response.json();
+}
+
+async function insertIntoDB(data) {
+  try {
+    const restaurant_name = data.name;
+    const category = data.category;
+    await db.exec(`INSERT INTO restaurants (restaurant_name, category) VALUES ("${restaurant_name}", "${category}")`);
+    console.log(`${restaurant_name} and ${category} inserted`);
+  }
+  catch (e) {
+    console.log('Error on insertion');
+    console.log(e);
+  }
+}
+
+async function databaseInitialize(dbSettings) {
+  try {
+    const db = await open(dbSettings);
+    await db.exec(`CREATE TABLE IF NOT EXISTS restaurants (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      restaurant_name TEXT,
+      category TEXT)
+      `);
+    const data = await dataFetch();
+    data.forEach((entry) => { insertIntoDB(entry) });
+    const test = await db.get('SELECT * FROM restaurants');
+    console.log('Success');
+  }
+  catch (e) {
+    console.log('Error loading Database');
+  }
+}
+
+// endpoints
 
 app.route('/api')
   .get((req, res) => {
@@ -33,6 +85,19 @@ app.route('/api')
     const json = await data.json();
     console.log(json);
     res.json(json);
+  });
+
+app.route('/sql')
+  .get((req, res) => {
+    console.log('GET detected');
+  })
+  .post(async (req, res) => {
+    console.log('POST request detected');
+    console.log('Form data in res.body', req.body);
+    
+    const db = await open(dbSettings);
+    const output = await query(db);
+    res.json(output);
   });
 
 app.listen(port, () => {
